48a35b58a325295912d0257e00ca264a5ab8b53f
4myc <missch3n@gmail.com>
2019-12-10 13:35:21 -0800

    Done.

--- db/migrate/20191210212559_create_dogs.rb
+++ db/migrate/20191210212559_create_dogs.rb
@@ -1,4 +1,5 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
+  #While the rollback (down) method is not included, it's implicit in the change method.
   def change
     create_table :dogs do |t|
       t.string :name
